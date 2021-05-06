61636739446b6321a9aee4284d9e6dcd6c676e56
Nathan Birch <birchn@byui.edu>
2021-05-05 21:25:23 -0600

    adding alarm clock app

---
+++ 05project/site.js
@@ -0,0 +1,4 @@
+let wakeUpImage =
+  'https://www.adam-eason.com/wp-content/uploads/2015/07/Fotolia_80486439_Subscription_Monthly_M.jpg';
+let backgroundColor = '#F7F96E';
+let alarmSound = 'assets/sleep.mp3';
